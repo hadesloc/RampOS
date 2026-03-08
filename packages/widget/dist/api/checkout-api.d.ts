c5d15136fe4873af0ec1d52a2440a911e2d932ac
hadesloc <hadeabountest1@gmail.com>
2026-03-08 19:48:58 +0700

    chore: stop tracking generated artifacts

--- packages/widget/dist/api/checkout-api.d.ts
+++
@@ -1,9 +0,0 @@
-import { CheckoutResult } from '../types';
-export interface CreatePayinIntentParams {
-    apiKey: string;
-    amount: number;
-    asset: string;
-    baseUrl?: string;
-    timeoutMs?: number;
-}
-export declare function createPayinIntent(params: CreatePayinIntentParams): Promise<CheckoutResult>;
--- packages/widget/dist/components/shared/Input.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    label?: string;
-    error?: string;
-    helpText?: string;
-}
-declare const Input: React.FC<InputProps>;
-export default Input;
